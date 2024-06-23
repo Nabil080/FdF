ac996950833560dcaa403c377bb70bd43e1bc0a3
Nabil080 <bellilanabil8@gmail.com>
2024-06-23 23:45:14 +0200

    defines.h for colors/keys, exit with q/esc

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 22:35:07 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/23 23:36:52 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/23 23:44:06 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,8 @@
 # define FDF_H
 
 # include <mlx.h>
-
 # include "libft.h"
-
-/*
-* colors
-*/
-# define GREEN 0x0000FF00
+# include "defines.h"
 
 typedef	struct s_data
 {
@@ -33,6 +28,8 @@ typedef	struct s_data
 void	exit_failure(void);
 
 void	exit_free(void *data);
+
+void	exit_free_all(t_data *data);
 /*
 * handler
 */
--- srcs/handler.c
+++ srcs/handler.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 23:31:44 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/23 23:36:10 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/23 23:44:08 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,13 @@ int	handle_mouse(int button, int x, int y, void *param)
 
 int	handle_input(int key, void *param)
 {
+	t_data	*data;
+
+	data = param;
 	ft_printf("key : %d\n", key);
-	(void)*param;
+	if (key == Q || key == ESC)
+		exit_free_all(data);
+	else
+		mlx_string_put(data->mlx, data->window, 20, 20, GREEN, (char *)&key);
 	return (1);
 }
