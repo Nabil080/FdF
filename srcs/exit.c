3a385e1a1b84e9c892dc98400600f3c9a6e624db
Nabil080 <bellilanabil8@gmail.com>
2024-06-24 00:44:43 +0200

    get map

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 23:31:04 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/23 23:44:07 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/24 00:39:18 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,12 @@ void	exit_free_all(t_data *data)
 	mlx_destroy_window(data->mlx, data->window);
 	free(data->mlx);
 	exit_free(data);
+}
+
+void	exit_arguments(void)
+{
+	ft_printf("Please give a valid .fdf file as argument\n");
+	ft_printf("Correct format : ./fdf <model>");
+	ft_printf("Exemple : ./fdf 42.fdf");
+	exit_failure();
 }
