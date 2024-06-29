98f234a6fe94398391018a9578a1f6743b6f28b9
Nabil080 <bellilanabil8@gmail.com>
2024-06-29 19:26:32 +0200

    get_lines done, manque convert to map

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:09:26 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/28 18:41:52 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/28 18:45:49 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	exit_args(void)
 {
-	ft_putendl_fd("Please give a valid .fdf file as argument",1);
-	ft_putendl_fd("Correct format:./fdf <model>",1);
-	ft_putendl_fd("Example :./fdf 42.fdf",1);
+	ft_putendl_fd("Please give a valid .fdf file as argument", 1);
+	ft_putendl_fd("Correct format:./fdf <model>", 1);
+	ft_putendl_fd("Example :./fdf 42.fdf", 1);
 	exit(EXIT_FAILURE);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:08:33 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/28 18:41:54 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/29 19:25:28 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int	main(int argc, char **argv)
 {
+	t_map	*map;
+
 	check_args(argc, argv);
+	map = get_map(argv[1]);
+	free(map);
+	return (0);
 }
