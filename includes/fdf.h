c4ab05e45170f7e9583057d18903c667cc18d55e
Nabil080 <nbellila@student.42.fr>
2024-07-02 05:24:51 +0200

    mlx init

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 22:35:07 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/02 04:09:14 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/02 04:59:46 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,14 @@ typedef struct s_map{
 	size_t	width;
 	int		**tab;
 }t_map;
+
+typedef struct t_data{
+	t_map	*map;
+	void	*mlx;
+	void	*win;
+	void	*img;
+	void	*adr;
+}t_data;
 /*parsing*/
 void	check_file(char *file);
 
@@ -30,11 +38,17 @@ t_map	*get_map(char *file);
 /*exit*/
 void	exit_args(void);
 
-void	exit_malloc(void);
-
 void	exit_error(char *str);
 
-void	exit_success(t_map *map);
+void	free_data(t_data *data);
+
+void	exit_success(t_data *data);
+
+void	free_map(t_map *map);
+/*init*/
+t_data	*init_data(t_map *map);
+
+void	*init_mlx(t_data *data);
 /*debug*/
 void	show_map(t_map *map);
 
--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:09:26 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/02 04:05:56 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/02 04:48:16 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,39 @@ void	exit_args(void)
 	exit(EXIT_FAILURE);
 }
 
-void	exit_malloc(void)
-{
-	exit_error("An allocation failed");
-}
-
 void	exit_error(char *str)
 {
 	ft_putendl_fd(str, 2);
 	exit(EXIT_FAILURE);
 }
 
-void	exit_success(t_map *map)
+void	exit_success(t_data *data)
+{
+	free_data(data);
+	exit(EXIT_SUCCESS);
+}
+
+void	free_data(t_data *data)
+{
+	if (!data)
+		return ;
+	if (data->map)
+		free_map(data->map);
+	if (!data->mlx)
+		return (free(data));
+	if (data->win)
+		mlx_destroy_window(data->mlx, data->win);
+	if (data->img)
+		free(data->img);
+	if (data->adr)
+		free(data->adr);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+	free(data);
+}
+
+void	free_map(t_map *map)
 {
 	free_2d((void **)map->tab, map->height);
 	free(map);
-	exit(EXIT_SUCCESS);
 }
