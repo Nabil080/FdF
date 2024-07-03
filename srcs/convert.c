4d0ab06e3c4e3abe5b6805fdca25e9f80043793b
Nabil080 <bellilanabil8@gmail.com>
2024-07-03 22:24:09 +0200

    norm

---
+++ srcs/convert.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 13:09:58 by nbellila          #+#    #+#             */
+/*   Updated: 2024/07/03 22:23:35 by nbellila         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	get_color(int z)
+{
+	if (z > 0)
+		return (GREEN);
+	return (WHITE);
+}
+
+int	get_spacing(t_data *data)
+{
+	if (data->map->width > data->map->height)
+		return ((data->width / (data->map->width)) * data->zoom);
+	else
+		return ((data->height / (data->map->height)) * data->zoom);
+}
+
+void	center(t_data *data, t_pos *pos, int spacing)
+{
+	if (data->map->height > data->map->width)
+	{
+		pos->x += (data->width - (data->map->width - 1) * spacing) / 2;
+		pos->y += spacing / 2;
+	}
+	else if (data->map->width > data->map->height)
+	{
+		pos->x += spacing / 2;
+		pos->y += (data->height - (data->map->height - 1) * spacing) / 2;
+	}
+	else
+	{
+		pos->x += (data->width - (data->map->width - 1) * spacing) / 2;
+		pos->y += (data->height - (data->map->height - 1) * spacing) / 2;
+	}
+}
+
+void	iso(int *x, int *y, int z)
+{
+	int	tmp_x;
+	int	tmp_y;
+
+	tmp_x = *x;
+	tmp_y = *y;
+	*x = (tmp_x - tmp_y) * cos(0.523599);
+	*y = (tmp_x + tmp_y) * sin(0.523599) - z;
+}
--- srcs/draw_utils.c
+++ srcs/draw_utils.c
@@ -5,53 +5,103 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/03 13:09:58 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/03 19:54:16 by nbellila         ###   ########.fr       */
+/*   Created: 2024/07/03 22:10:33 by nbellila          #+#    #+#             */
+/*   Updated: 2024/07/03 22:22:52 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	get_color(int z)
+static t_vars	get_bresenham_vars(t_pos a, t_pos b, int isLow)
 {
-	if (z > 0)
-		return (GREEN);
-	return (WHITE);
+	t_vars	vars;
+
+	vars.dx = b.x - a.x;
+	vars.dy = b.y - a.y;
+	vars.i = 1;
+	if (isLow && vars.dy < 0)
+	{
+		vars.i = -1;
+		vars.dy = -vars.dy;
+	}
+	else if (vars.dx < 0)
+	{
+		vars.i = -1;
+		vars.dx = -vars.dx;
+	}
+	vars.d = 2 * vars.dx - vars.dy;
+	return (vars);
 }
 
-int	get_spacing(t_data *data)
+static void	bresenham_low_slope(t_data *data, t_pos a, t_pos b)
 {
-	if (data->map->width > data->map->height)
-		return ((data->width / (data->map->width)) * data->zoom);
+	t_pos	current;
+	t_vars	vars;
+
+	vars = get_bresenham_vars(a, b, 1);
+	current = a;
+	while (current.x < b.x)
+	{
+		if (b.x - current.x <= (b.x - a.x) / 2)
+			current.z = b.z;
+		put_pixel(data, &current, 0);
+		if (vars.d > 0)
+		{
+			current.y += vars.i;
+			vars.d = vars.d + (2 * (vars.dy - vars.dx));
+		}
+		else
+			vars.d = vars.d + 2 * vars.dy;
+		current.x++;
+	}
+}
+
+static void	bresenham_high_slope(t_data *data, t_pos a, t_pos b)
+{
+	t_pos	current;
+	t_vars	vars;
+
+	vars = get_bresenham_vars(a, b, 0);
+	current = a;
+	while (current.y < b.y)
+	{
+		if (b.y - current.y <= (b.y - a.y) / 2)
+			current.z = b.z;
+		put_pixel(data, &current, 0);
+		if (vars.d > 0)
+		{
+			current.x += vars.i;
+			vars.d = vars.d + (2 * (vars.dx - vars.dy));
+		}
+		else
+			vars.d = vars.d + 2 * vars.dx;
+		current.y++;
+	}
+}
+
+void	draw_line(t_data *data, t_pos a, t_pos b)
+{
+	t_pos	x;
+	t_pos	y;
+
+	x = a;
+	y = b;
+	if (abs(b.y - a.y) < abs(b.x - a.x))
+	{
+		if (a.x > b.x)
+		{
+			x = b;
+			y = b;
+		}
+		bresenham_low_slope(data, x, y);
+	}
 	else
-		return ((data->height / (data->map->height)) * data->zoom);
-}
-
-void	center(t_data *data, t_pos *pos, int spacing)
-{
-	if (data->map->height > data->map->width)
 	{
-		pos->x += (data->width - (data->map->width - 1) * spacing) / 2;
-		pos->y += spacing / 2;
+		if (a.y > b.y)
+		{
+			x = b;
+			y = b;
+		}
+		bresenham_high_slope(data, x, y);
 	}
-	else if (data->map->width > data->map->height)
-	{
-		pos->x += spacing / 2;
-		pos->y += (data->height - (data->map->height - 1) * spacing) / 2;
-	}
-	else
-	{
-		pos->x += (data->width - (data->map->width - 1) * spacing) / 2;
-		pos->y += (data->height - (data->map->height - 1) * spacing) / 2;
-	}
-}
-void	iso(int *x, int *y, int z)
-{
-    int	previous_x;
-    int	previous_y;
-
-    previous_x = *x;
-    previous_y = *y;
-    *x = (previous_x - previous_y) * cos(0.523599);
-    *y = -z + (previous_x + previous_y) * sin(0.523599);
 }
